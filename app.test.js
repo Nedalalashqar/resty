ec3698c5f4f47e35cf93b32099098504b7fa414b
nedalalashqar <nedal.r.alashqar@gmail.com>
2021-09-14 20:21:43 +0300

    add

---
+++ app.test.js
@@ -0,0 +1,11 @@
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import History from './components/history';
+import '@testing-library/jest-dom/extend-expect';
+it('render results', () => {
+  const result = ['https://pokeapi.co/api/v2/pokemon','get']
+  render(<History history={result} />);
+  const items = screen.getByTestId(0);
+  expect(items).toHaveTextContent('https://pokeapi.co/api/v2/pokemon');
+  const _items = screen.getByTestId(1);
+  expect(_items).toHaveTextContent('get');
+});
